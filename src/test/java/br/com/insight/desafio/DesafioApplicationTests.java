7887facbc538c65d4f4e1f4571d3ae3340913e76
Leonardo Almeida <leonardoaesilva@outlook.com>
2024-06-02 23:15:27 -0300

    envio inicial

---
+++ src/test/java/br/com/insight/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insight.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
