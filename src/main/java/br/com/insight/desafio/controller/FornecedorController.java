3797e7596578b28ab6010a497eaafd0e10f0c137
Leonardo Almeida <leonardoaesilva@outlook.com>
2024-06-03 23:05:58 -0300

    alteração do método da rota inicial para listagem

--- src/main/java/br/com/insight/desafio/controller/FornecedorController.java
+++ src/main/java/br/com/insight/desafio/controller/FornecedorController.java
@@ -1,12 +1,19 @@
 package br.com.insight.desafio.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.insight.desafio.model.FornecedorModel;
+import br.com.insight.desafio.service.FornecedorService;
+
 @RestController
 public class FornecedorController {
+    @Autowired
+    private FornecedorService fs;
+
     @GetMapping("/")
-    public String resp() {
-        return "Olá!";
+    public Iterable<FornecedorModel> listar() {
+        return fs.listarTodos();
     }
 }
