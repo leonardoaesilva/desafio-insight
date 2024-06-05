f02f0eff04faa7bf3a164e5dc8c81d6e8ec7df6d
Leonardo Almeida <leonardoaesilva@outlook.com>
2024-06-04 22:43:54 -0300

    implementação de método e rota para adição de fornecedor

--- src/main/java/br/com/insight/desafio/controllers/FornecedorController.java
+++ src/main/java/br/com/insight/desafio/controllers/FornecedorController.java
@@ -1,11 +1,15 @@
 package br.com.insight.desafio.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.insight.desafio.models.FornecedorModel;
 import br.com.insight.desafio.services.FornecedorService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class FornecedorController {
@@ -16,4 +20,10 @@ public class FornecedorController {
     public Iterable<FornecedorModel> listar() {
         return fs.listarTodos();
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> cadastrar(@RequestBody FornecedorModel fornecedorNovo) {
+        return fs.cadastrarFornecedor(fornecedorNovo);
+    }
+    
 }
--- src/main/java/br/com/insight/desafio/services/FornecedorService.java
+++ src/main/java/br/com/insight/desafio/services/FornecedorService.java
@@ -1,6 +1,8 @@
 package br.com.insight.desafio.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.insight.desafio.models.FornecedorModel;
@@ -13,5 +15,17 @@ public class FornecedorService {
 
     public Iterable<FornecedorModel> listarTodos() {
         return fr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrarFornecedor(FornecedorModel fornecedorNovo) {
+        if (fornecedorNovo.getCnpj().equals("")) {
+            String resp = "CNPJ não pode ficar em branco.";
+            return new ResponseEntity<String>(resp, HttpStatus.BAD_REQUEST);
+        } else if (fornecedorNovo.getNomeFantasia().equals("")) {
+            String resp = "Nome Fantasia não pode ficar em branco.";
+            return new ResponseEntity<String>(resp, HttpStatus.BAD_REQUEST);
+        }
+        
+        return new ResponseEntity<FornecedorModel>(fr.save(fornecedorNovo), HttpStatus.CREATED);
     }
 }
