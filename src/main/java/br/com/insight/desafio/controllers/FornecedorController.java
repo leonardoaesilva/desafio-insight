d37648a7e93d637eaec4c180e6ce6bf97b4e0fce
Leonardo Almeida <leonardoaesilva@outlook.com>
2024-06-09 16:05:28 -0300

    permitindo comunicação entre portas e testando a requisição à api

--- src/main/java/br/com/insight/desafio/controllers/FornecedorController.java
+++ src/main/java/br/com/insight/desafio/controllers/FornecedorController.java
@@ -2,6 +2,7 @@ package br.com.insight.desafio.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class FornecedorController {
     @Autowired
     private FornecedorService fornecedorService;
--- src/main/resources/view/src/App.js
+++ src/main/resources/view/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Form from './Form';
 import Tabela from './Tabela';
 
 function App() {
-  
   const[btnCadastrar, setBtnCadastrar] = useState(true);
+  const[fornecedores, setFornecedor] = useState([]);
+
+  useEffect(() => { fetch("http://localhost:8080/api/fornecedores").then(resp => resp.json()).then(jsonResp => setFornecedor(jsonResp)); }, []);
+
   return (
     <div>
       <Form acao={btnCadastrar} />
       <Tabela />
+      <div>
+        <p>{JSON.stringify(fornecedores)}</p>
+      </div>
     </div>
   );
 }
