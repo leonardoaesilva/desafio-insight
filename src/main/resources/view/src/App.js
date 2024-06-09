6aa82e89a1c5b549907bd4cadf6830acdec5eed2
Leonardo Almeida <leonardoaesilva@outlook.com>
2024-06-09 15:51:01 -0300

    definindo a visibilidade dos botões

--- src/main/resources/view/src/App.js
+++ src/main/resources/view/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 import Form from './Form';
 import Tabela from './Tabela';
 
 function App() {
+  
+  const[btnCadastrar, setBtnCadastrar] = useState(true);
   return (
     <div>
-      <Form />
+      <Form acao={btnCadastrar} />
       <Tabela />
     </div>
   );
--- src/main/resources/view/src/Form.js
+++ src/main/resources/view/src/Form.js
@@ -1,13 +1,18 @@
-function Form() {
+function Form({acao}) {
     return(
         <form>
             <input type='text' placeholder='Nome' className='form-control' />
             <input type='text' placeholder='CNPJ' className='form-control' />
 
-            <input type='button' value='Cadastrar' className='btn btn-success' />
-            <input type='button' value='Cancelar' className='btn btn-secondary' />
-            <input type='button' value='Editar' className='btn btn-primary' />
-            <input type='button' value='Remover' className='btn btn-danger' />
+        {
+            acao ? 
+                <input type='button' value='Cadastrar' className='btn btn-success' /> :
+                <div>
+                    <input type='button' value='Cancelar' className='btn btn-secondary' />
+                    <input type='button' value='Editar' className='btn btn-primary' />
+                    <input type='button' value='Remover' className='btn btn-danger' />
+                </div>
+        }
         </form>
     )
 }
