35238651f179fb73658064dc1737f4c1f5f70918
Leonardo Almeida <leonardoaesilva@outlook.com>
2024-06-09 16:21:21 -0300

    início da passagem de argumentos para o objeto a ser salvo

--- src/main/resources/view/src/App.js
+++ src/main/resources/view/src/App.js
@@ -4,14 +4,19 @@ import Form from './Form';
 import Tabela from './Tabela';
 
 function App() {
+  
+  const fornecedor = { codigoFornecedor:0, nome:'', cnpj:'' }
+
   const[btnCadastrar, setBtnCadastrar] = useState(true);
   const[fornecedores, setFornecedor] = useState([]);
+  const[objFornecedor, setObjFornecedor] = useState(fornecedor);
 
   useEffect(() => { fetch("http://localhost:8080/api/fornecedores").then(resp => resp.json()).then(jsonResp => setFornecedor(jsonResp)); }, []);
 
   return (
     <div>
       <Form acao={btnCadastrar} />
+      <p>{JSON.stringify(objFornecedor)}</p>
       <Tabela lista={fornecedores} />
     </div>
   );
